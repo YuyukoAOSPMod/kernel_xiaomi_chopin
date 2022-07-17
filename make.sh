1cc8e7984ba355397cc1d69d8568320e366e7fe4
Yuyuko1024 <dingwenxuan4@gmail.com>
2022-07-17 11:21:40 +0800

    scripts: Use custom defconfig
    Signed-off-by: Yuyuko1024 <dingwenxuan4@gmail.com>

--- make.sh
+++ make.sh
@@ -4,5 +4,5 @@ export ARCH=arm64
 export SUBARCH=arm64
 export OUTPUT=out
 make CC=$CLANG_PATH/bin/clang CLANG_TRIPLE=$CLANG_PATH/bin/aarch64-linux-gnu- CROSS_COMPILE=$CLANG_PATH/bin/aarch64-linux-gnu- O=$OUTPUT mrproper
-make CC=$CLANG_PATH/bin/clang CLANG_TRIPLE=$CLANG_PATH/bin/aarch64-linux-gnu- CROSS_COMPILE=$CLANG_PATH/bin/aarch64-linux-gnu- O=$OUTPUT chopin_user_defconfig
+make CC=$CLANG_PATH/bin/clang CLANG_TRIPLE=$CLANG_PATH/bin/aarch64-linux-gnu- CROSS_COMPILE=$CLANG_PATH/bin/aarch64-linux-gnu- O=$OUTPUT chopin_maribel_defconfig
 make CC=$CLANG_PATH/bin/clang CLANG_TRIPLE=$CLANG_PATH/bin/aarch64-linux-gnu- CROSS_COMPILE=$CLANG_PATH/bin/aarch64-linux-gnu- O=$OUTPUT -j16
