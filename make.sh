27c515b2df9eedddd1e8f72ecb228d75511d7a0c
Yuyuko1024 <dingwenxuan4@gmail.com>
2022-07-17 10:40:57 +0800

    kernel: Add make scripts
    Signed-off-by: Yuyuko1024 <dingwenxuan4@gmail.com>

---
+++ make.sh
@@ -0,0 +1,8 @@
+export CLANG_PATH=~/general-clang
+export PATH=${CLANG_PATH}/bin:${PATH}
+export ARCH=arm64
+export SUBARCH=arm64
+export OUTPUT=out
+make CC=$CLANG_PATH/bin/clang CLANG_TRIPLE=$CLANG_PATH/bin/aarch64-linux-gnu- CROSS_COMPILE=$CLANG_PATH/bin/aarch64-linux-gnu- O=$OUTPUT mrproper
+make CC=$CLANG_PATH/bin/clang CLANG_TRIPLE=$CLANG_PATH/bin/aarch64-linux-gnu- CROSS_COMPILE=$CLANG_PATH/bin/aarch64-linux-gnu- O=$OUTPUT chopin_user_defconfig
+make CC=$CLANG_PATH/bin/clang CLANG_TRIPLE=$CLANG_PATH/bin/aarch64-linux-gnu- CROSS_COMPILE=$CLANG_PATH/bin/aarch64-linux-gnu- O=$OUTPUT -j16
